2daadbc482d6096ce83dcbc138e6934264851a50
avlokraj2372 <rajavlok@gmail.com>
2025-07-24 23:18:16 +0530

    reset password is working now

--- Backend/controllers/resetPassword.js
+++ Backend/controllers/resetPassword.js
@@ -25,7 +25,7 @@ exports.resetPasswordToken = async (req, res) => {
     console.log("DETAILS", updatedDetails)
 
     // const url = `http://localhost:3000/update-password/${token}`
-    const url = `https://study-notion-git-main-avloks-projects-ad073ff4.vercel.app/update-password/${token}`
+    const url = `https://study-notion-six-weld.vercel.app/update-password/${token}`
 
     await mailSender(
       email,
